56d52600aac596f04d67747670a2551ef6595c2a
ajperry4218 <35818537+ajperry4218@users.noreply.github.com>
2018-01-25 16:26:18 -0500

    Update Hero.cs

--- Crystal_Transit/Crystal_Transit/Sprites/Hero.cs
+++ Crystal_Transit/Crystal_Transit/Sprites/Hero.cs
@@ -16,6 +16,29 @@ namespace Crystal_Transit
             KeyboardState keyboardState = Keyboard.GetState();
             UserInput(keyboardState);
         }
+        GamePadState gamePad State = GamePad.GetState (PlayerIndex.One);
+		if (gamePadState.IsConnected)
+		{
+			
+`		}
+		if(gamepadState.Buttons.X == ButtonState>Pressed
+		{
+		
+		}
+		float  maxSpeed = 0.1f;
+		float  changeInAngle = gamepadState.Thumbsticks.Left.X * max speed;
+		Angle += changeInAngle
+`		{
+		
+		}
+		float leftTriggerValue = gamePadState.Triggers.Left;
+		{
+		
+		}
+		GamePadState newstate =GamePad.GetState(PlayerIndex.One);
+		if(newState.Buttons.X == ButtonState.Pressed &&
+					oldState.Buttons.X == ButtonState.Released
+
 
         void UserInput(KeyboardState keyboardState)// moves the hero by key
         {
